e6563d3ff35bd913ebc9bb8884623117e49e802b
Pedro Moreira <pedro.moreira@emma-sleep.com>
2024-08-26 19:23:50 +0000

    initial steps

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <list-item 
+      v-for="resource in storedResources" 
+      :key="resource.id" 
+      :resource="resource"
+    ></list-item>
+  </ul>
+</template>
+
+<script>
+import ListItem from './components/ListItem.vue';
+
+export default {
+  components: {
+    ListItem
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'offical-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        }
+      ]
+    }
+  }
+}
+</script>
